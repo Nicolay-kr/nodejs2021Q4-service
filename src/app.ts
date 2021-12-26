f664a241eecacd2e60eb1800b4680a11f39b08a5
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2021-12-26 22:07:04 +0300

    feat: update app

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import YAML from 'yamljs';
 import { router as userRouter } from './resources/users/user.router';
 import { router as boardRouter } from './resources/boards/board.router';
 import { router as taskRouter } from './resources/tasks/task.router';
+import {morganLog} from './middleware/morgan';
+import errorHandler from './middleware/unhandledErrorsHandler';
+import handleException from './middleware/uncaughtErrorsHandler';
 
 export {}
 
@@ -14,6 +17,8 @@ const app: Application = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
+app.use(morganLog);
+handleException();
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
@@ -28,5 +33,7 @@ app.use('/', (req: Request, res: Response, next: NextFunction ) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
+
+app.use(errorHandler);
 
 export {app};
