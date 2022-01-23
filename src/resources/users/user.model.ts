7905c3ed8a3073b6647ee13d0e92eb0ee1594cde
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-01-23 22:15:36 +0300

    feat: update user model

--- src/resources/users/user.model.ts
+++ src/resources/users/user.model.ts
@@ -1,5 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm";
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import {JWT_SECRET_KEY} from '../../common/config';
 
 // const uuid = require('uuid');
 
@@ -16,7 +18,7 @@ export class User implements IUser{
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('varchar', {length: 255, default: 'login'})
+  @Column('varchar', {length: 255, default: 'login', unique: true})
   login: string;
 
   @Column('varchar', {length: 255, default: 'user'})
@@ -28,5 +30,12 @@ export class User implements IUser{
   @BeforeInsert()
   generatePasswordHash() {
     this.password = bcrypt.hashSync(this.password, 10);
+  }
+
+  async generateAuthToken() {
+    const token = jwt.sign({ userId: this.id, login: this.login }, JWT_SECRET_KEY!, {
+      expiresIn: '1h',
+    });
+    return token;
   }
 }
