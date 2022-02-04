4f483df156293e047355036b45020e091e2c4552
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-02-04 15:56:43 +0300

    feat: add main resources

---
+++ nest-app/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './resources/users/users.module';
+import { BoardsModule } from './resources/boards/boards.module';
+import { TasksModule } from './resources/tasks/tasks.module';
+
+@Module({
+  imports: [UsersModule, BoardsModule, TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
